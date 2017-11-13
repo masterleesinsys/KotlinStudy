b49de48512c4337ba4e1db8395b2c6e38bbb9684
masterleesinsys <m15896847719@163.com>
2017-11-13 10:39:04 +0800

    Create TestActivity.java

---
+++ app/src/main/java/fly/kotlinstudy/cm/activity/TestActivity.java
@@ -0,0 +1,11 @@
+package fly.kotlinstudy.cm.activity;
+
+import android.os.Bundle;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
